7ddfd2a07e010600b01ee1abe9e0a9b6f1e3cabf
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-10-22 20:41:22 +0000

    mm

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 # --- PAGE SETUP ---
-about_page = st.Page(
+Architecture_page = st.Page(
     "pages/Architecture.py",
     title="Architecture",
     icon=":material/home:",
@@ -23,15 +23,15 @@ project_2_page = st.Page(
 # --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
-        "Info": [about_page],
+        "Info": [Architecture_page],
         "Projects": [project_1_page, project_2_page],
     }
 )
 
 
 # --- SHARED ON ALL PAGES ---
-st.logo("src/codingisfun_logo.png")
-st.sidebar.markdown("Made with ❤️ by [Sven](https://youtube.com/@codingisfun)")
+# st.logo("src/codingisfun_logo.png")
+# st.sidebar.markdown("Made with ❤️ by [Sven](https://youtube.com/@codingisfun)")
 
 
 # --- RUN NAVIGATION ---
