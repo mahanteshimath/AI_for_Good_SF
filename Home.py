540947235dc2425a81781d5ab74e32b3adbfa23a
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-10-22 20:21:31 +0000

    test

---
+++ Home.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+
+# --- PAGE SETUP ---
+about_page = st.Page(
+    "pages/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    "pages/sales_dashboard.py",
+    title="Sales Dashboard",
+    icon=":material/bar_chart:",
+)
+project_2_page = st.Page(
+    "pages/chatbot.py",
+    title="Chat Bot",
+    icon=":material/smart_toy:",
+)
+
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page, project_2_page],
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+st.logo("assets/codingisfun_logo.png")
+st.sidebar.markdown("Made with ❤️ by [Sven](https://youtube.com/@codingisfun)")
+
+
+# --- RUN NAVIGATION ---
+pg.run()
