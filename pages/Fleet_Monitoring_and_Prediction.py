e439896dbbffbfeab5a10b76c7ad1adeaca69afc
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-05 22:38:24 +0530

    mm

--- pages/Fleet_Monitoring_and_Prediction.py
+++ pages/Fleet_Monitoring_and_Prediction.py
@@ -2,3 +2,26 @@ import streamlit as st
 import pandas as pd 
 
 
+st.title(":blue[By Road Passengers predictions]")
+st.write("\n")
+
+st.write("\n")
+st.write("\n")
+
+st.write("\n")
+st.write("\n")
+st.write("\n")
+
+st.write("\n")
+st.title(":blue[By Air Passengers predictions]")
+st.write("\n")
+
+st.write("\n")
+st.write("\n")
+
+st.write("\n")
+st.write("\n")
+st.write("\n")
+
+st.write("\n")
+st.title(":blue[By Water Passengers predictions]")
