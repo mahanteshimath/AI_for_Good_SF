833cf352bb11d5b2ee2839ca52d22111fdec580a
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-05 19:27:05 +0530

    mm

--- pages/Hypothesis.py
+++ pages/Hypothesis.py
@@ -18,25 +18,16 @@ with col2:
     )
 
 st.write("\n")
-st.subheader("Experience & Qualifications", anchor=False)
-st.write(
-    """
-    - 7 Years experience extracting actionable insights from data
-    - Strong hands-on experience and knowledge in Python and Excel
-    - Good understanding of statistical principles and their respective applications
-    - Excellent team-player and displaying a strong sense of initiative on tasks
-    """
-)
+# st.subheader("Experience & Qualifications", anchor=False)
+# st.write(
+#     """
+#     - 7 Years experience extracting actionable insights from data
+#     - Strong hands-on experience and knowledge in Python and Excel
+#     - Good understanding of statistical principles and their respective applications
+#     - Excellent team-player and displaying a strong sense of initiative on tasks
+#     """
+# )
 st.write("\n")
-st.subheader("Hard Skills", anchor=False)
-st.write(
-    """
-    - Programming: Python (Scikit-learn, Pandas), SQL, VBA
-    - Data Visualization: PowerBi, MS Excel, Plotly
-    - Modeling: Logistic regression, linear regression, decision trees
-    - Databases: Postgres, MongoDB, MySQL
-    """
-)
 
 
 
