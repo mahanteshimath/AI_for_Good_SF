a8dde3265b800aae8ebfa6b62394055e0e2ad011
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-05 18:48:31 +0530

    M

--- pages/Architecture.py
+++ pages/Architecture.py
@@ -11,4 +11,4 @@ with open("./src/Architectureembed.txt", "r") as file:
 
 
 # Display the HTML content as an embedded diagram in Streamlit
-components.html(html_content, width=800, height=600, scrolling=True)
+components.html(html_content, width=1200, height=800, scrolling=True)
