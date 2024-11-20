d5806b50e15f84c4e4da9807317769d9116ee980
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-20 18:42:25 +0530

    Update Hypothesis.py

--- pages/Hypothesis.py
+++ pages/Hypothesis.py
@@ -98,7 +98,7 @@ def add_custom_css():
     st.markdown("""
         <style>
         .flashing-title {
-            font-size: 1.5em;
+            font-size: 2em;
             font-weight: bold;
             color: #4CAF50;
             animation: flash 2s infinite;
