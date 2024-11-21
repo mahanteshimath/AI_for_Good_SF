b8a56eefb3918147de9e978db17b0bc6f73d4971
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-22 03:36:17 +0530

    mmm

--- pages/Snowflake_Powered_Accident_Analysis_bot.py
+++ pages/Snowflake_Powered_Accident_Analysis_bot.py
@@ -40,109 +40,6 @@ st.title(":blue[📈Analysis with Snowflake Cortex & RAG ] :speech_balloon:")
     # Get active Snowflake session
 session = get_snowflake_session()
     
-
-
-    # Create columns for layout
-col1, col2, col3 = st.columns([1, 0.05, 1])
-
-with col1:
-    st.write("### Configuration Options")
-    config_options()
-    # Add custom CSS to style the button
-    st.markdown(
-        """
-        <style>
-        .glowing-button {
-            background-color: red;
-            color: white;
-            border: none;
-            padding: 10px 20px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 8px;
-            box-shadow: 0 0 5px red, 0 0 10px red, 0 0 15px red;
-            transition: box-shadow 0.3s ease-in-out;
-        }
-        .glowing-button:hover {
-            box-shadow: 0 0 20px red, 0 0 30px red, 0 0 40px red;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    @st.dialog("How RAG works in Snowflake?")
-    def show_dialog():
-        markdown_content = '''
-            ![rag in snowflake](https://publish-p57963-e462109.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--7e5d3595-a32c-44de-86ca-cfa2883d475e/rag1.png?preferwebp=true&width=1440&quality=85)
-        '''
-        st.markdown(markdown_content)
-
-        # Create a button using Streamlit
-        if st.button("How RAG works in Snowflake?", key="rag_button",type="secondary"):
-            show_dialog()
-
-    with col2:
-        st.markdown(
-            """
-            <style>
-            .divider {
-                height: 100%;
-                width: 1px;
-                background-color: #e0e0e0;
-                margin: 0 10px;
-            }
-            </style>
-            <div class="divider"></div>
-            """,
-            unsafe_allow_html=True
-        )
-
-    with col3:
-        st.write("### Documents Available")
-        st.write("This is the list of documents you already have and that will be used to answer your questions:")
-
-        # Query to list available documents
-        docs_available = session.sql("ls @docs").collect()
-        list_docs = []
-        for doc in docs_available:
-            list_docs.append(doc["name"])
-        st.dataframe(list_docs)
-
-    st.divider()
-
-    init_messages()
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Accept user input for questions
-    if question := st.chat_input("Chat with any docs"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": question})
-
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(question)
-
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            question = question.replace("'", "")
-            with st.spinner(f"{st.session_state.model_name} thinking..."):
-                response = complete(session, question)
-                res_text = response[0].RESPONSE
-                res_text = res_text.replace("'", "")
-                message_placeholder.markdown(res_text)
-
-        st.session_state.messages.append({"role": "assistant", "content": res_text})
-
 # Configuration options (now on the main page)
 def config_options():
     # Model selection
@@ -150,8 +47,7 @@ def config_options():
         'mixtral-8x7b', 'snowflake-arctic', 'mistral-large',
         'llama3-8b', 'llama3-70b', 'reka-flash', 
         'mistral-7b', 'llama2-70b-chat', 'gemma-7b'), key="model_name")
-
-    # Chat history usage
+   # Chat history usage
     st.checkbox('Do you want that I remember the chat history?', key="use_chat_history", value=True)
 
     # Debug option
@@ -255,6 +151,112 @@ def complete(session, myquestion):
     cmd = "SELECT snowflake.cortex.complete(?, ?) AS response"
     df_response = session.sql(cmd, params=[st.session_state.model_name, prompt]).collect()
     return df_response
+
+    # Create columns for layout
+col1, col2, col3 = st.columns([1, 0.05, 1])
+
+with col1:
+    
+    st.write("### Configuration Options")
+    config_options()
+    # Add custom CSS to style the button
+    st.markdown(
+        """
+        <style>
+        .glowing-button {
+            background-color: red;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 8px;
+            box-shadow: 0 0 5px red, 0 0 10px red, 0 0 15px red;
+            transition: box-shadow 0.3s ease-in-out;
+        }
+        .glowing-button:hover {
+            box-shadow: 0 0 20px red, 0 0 30px red, 0 0 40px red;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    @st.dialog("How RAG works in Snowflake?")
+    def show_dialog():
+        markdown_content = '''
+            ![rag in snowflake](https://publish-p57963-e462109.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--7e5d3595-a32c-44de-86ca-cfa2883d475e/rag1.png?preferwebp=true&width=1440&quality=85)
+        '''
+        st.markdown(markdown_content)
+
+        # Create a button using Streamlit
+        if st.button("How RAG works in Snowflake?", key="rag_button",type="secondary"):
+            show_dialog()
+
+    with col2:
+        st.markdown(
+            """
+            <style>
+            .divider {
+                height: 100%;
+                width: 1px;
+                background-color: #e0e0e0;
+                margin: 0 10px;
+            }
+            </style>
+            <div class="divider"></div>
+            """,
+            unsafe_allow_html=True
+        )
+
+    with col3:
+        st.write("### Documents Available")
+        st.write("This is the list of documents you already have and that will be used to answer your questions:")
+
+        # Query to list available documents
+        docs_available = session.sql("ls @docs").collect()
+        list_docs = []
+        for doc in docs_available:
+            list_docs.append(doc["name"])
+        st.dataframe(list_docs)
+
+    st.divider()
+
+    init_messages()
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input for questions
+    if question := st.chat_input("Chat with any docs"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": question})
+
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(question)
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            question = question.replace("'", "")
+            with st.spinner(f"{st.session_state.model_name} thinking..."):
+                response = complete(session, question)
+                res_text = response[0].RESPONSE
+                res_text = res_text.replace("'", "")
+                message_placeholder.markdown(res_text)
+
+        st.session_state.messages.append({"role": "assistant", "content": res_text})
+
+
+
+ 
 
 
 
