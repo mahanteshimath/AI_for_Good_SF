3d1a96835451dc088464d4dfeb59a2fd02dbd8fe
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-23 13:58:35 +0530

    Final

--- pages/AI_Civil_Engineer.py
+++ pages/AI_Civil_Engineer.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.components.v1.html(
     """
     <iframe 
-        src="https://montys-multipdf-chat.streamlit.app/?embed=true" 
+        src="https://montys-multipdf-chat.streamlit.app/?embed=true,embed_options=disable_scrolling,dark_theme" 
         style="height: 800px; width: 100%; border: none;">
     </iframe>
     """,
