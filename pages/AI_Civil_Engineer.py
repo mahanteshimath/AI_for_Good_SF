9df10404b3c7c68e4b19a0f1f171883a1252335e
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-23 14:20:04 +0530

    mm

--- pages/AI_Civil_Engineer.py
+++ pages/AI_Civil_Engineer.py
@@ -1,11 +1,42 @@
+# import streamlit as st
+
+# st.components.v1.html(
+#     """
+#     <iframe 
+#         src="https://montys-multipdf-chat.streamlit.app//?embed=true&embed_options=show_colored_line&embed_options=disable_scrolling" 
+#         style="height: 800px; width: 100%; border: none;">
+#     </iframe>
+#     """,
+#     height=800,
+# )
+
+
 import streamlit as st
 
+# Embed the external app and make the iframe fit the page
 st.components.v1.html(
     """
-    <iframe 
-        src="https://montys-multipdf-chat.streamlit.app//?embed=true&embed_options=show_colored_line&embed_options=disable_scrolling" 
-        style="height: 800px; width: 100%; border: none;">
-    </iframe>
+    <style>
+        .iframe-container {
+            position: relative;
+            width: 100%;
+            height: 100vh; /* Full viewport height */
+            overflow: hidden;
+        }
+        .iframe-container iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+    <div class="iframe-container">
+        <iframe 
+            src="https://montys-multipdf-chat.streamlit.app//?embed=true&embed_options=show_colored_line">
+        </iframe>
+    </div>
     """,
-    height=800,
+    height=0,  # Let the CSS handle the height
 )
