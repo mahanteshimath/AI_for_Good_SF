b8db2ff4e5d506aee2c701555f2e7c633fc8ee1b
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-23 14:02:39 +0530

    mm

--- pages/AI_Civil_Engineer.py
+++ pages/AI_Civil_Engineer.py
@@ -4,8 +4,8 @@ st.components.v1.html(
     """
     <iframe 
         src="https://montys-multipdf-chat.streamlit.app/?embed=true" 
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;">
+        style="height: 100%; width: 100%; border: none;">
     </iframe>
     """,
-    height=0,  # Height is controlled entirely by CSS
+    height=800,
 )
