c3bd9bf56f3b098a8f34cfcaf66ec4878ecd48b2
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-23 15:55:14 +0530

    all pages alined

--- Home.py
+++ Home.py
@@ -21,7 +21,7 @@ st.set_page_config(
   initial_sidebar_state="expanded",
 ) 
 
-# --- PAGE SETUP ---
+# --- Info ---
 Hypothesis_page = st.Page(
     "pages/Hypothesis.py",
     title="Hypothesis",
@@ -33,6 +33,19 @@ Architecture_page = st.Page(
     title="Architecture",
     icon=":material/home:"
 
+)
+
+#What is wrong with Indian infra
+
+problem = st.Page(
+    "pages/Infra_Projects_Failure_Reasons.py",
+    title="Infra Projects Failure Reasons",
+    icon=":material/construction:",
+)
+AI_Civil = st.Page(
+    "pages/AI_Civil_Engineer.py",
+    title="AI Civil Engineer",
+    icon=":material/smart_toy:",
 )
 
 
@@ -55,6 +68,12 @@ project_3_page = st.Page(
     icon=":material/flight:",
 )
 
+AQI = st.Page(
+    "pages/Realtime_AQI_Across_India.py",
+    title="Realtime AQI Across India",
+    icon=":material/aq:",
+)
+
 #"SF-MP Social impact"
 project_4_page = st.Page(
     "pages/Indian_Roads.py",
@@ -63,22 +82,7 @@ project_4_page = st.Page(
 )
 
  #"Open Government Data"
-road_page = st.Page(
-    "pages/Accident_Severity_Prediction_Using_AI.py",
-    title="Accident Severity Prediction_Using AI",
-    icon=":material/car_crash:",
-)
 
-Accidents_page = st.Page(
-    "pages/Accidents_Analysis_Prevention_Using_AI.py",
-    title="Accidents Analysis Prevention Using AI",
-    icon=":material/car_crash:",
-)
-AQI = st.Page(
-    "pages/Realtime_AQI_Across_India.py",
-    title="Realtime AQI Across India",
-    icon=":material/aq:",
-)
 chatbot = st.Page(
     "pages/Snowflake_Powered_Accident_Analysis_bot.py",
     title="Snowflake Powered Accident Analysis bot",
@@ -86,24 +90,26 @@ chatbot = st.Page(
 )
 
 
+Accidents_page = st.Page(
+    "pages/Accidents_Analysis_Prevention_Using_AI.py",
+    title="Accidents Analysis Prevention Using AI",
+    icon=":material/car_crash:",
+)
+road_page = st.Page(
+    "pages/Accident_Severity_Prediction_Using_AI.py",
+    title="Accident Severity Prediction_Using AI",
+    icon=":material/car_crash:",
+)
 
-problem = st.Page(
-    "pages/Infra_Projects_Failure_Reasons.py",
-    title="Infra Projects Failure Reasons",
-    icon=":material/construction:",
-)
-AI_Civil = st.Page(
-    "pages/AI_Civil_Engineer.py",
-    title="AI Civil Engineer",
-    icon=":material/smart_toy:",
-)
+
+
 
 pg = st.navigation(
     {
         "Info": [Hypothesis_page,Architecture_page],
         "What is wrong with Indian infra": [problem,AI_Civil],
         "SF-MP Social impact Data": [project_4_page,project_1_page,project_3_page,project_2_page,AQI],
-        "Open Government Data":[chatbot,road_page,Accidents_page],
+        "Open Government Data":[chatbot,Accidents_page,road_page],
     }
 )
 
