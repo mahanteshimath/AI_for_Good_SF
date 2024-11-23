5bbe8c90189651e4921b30eec7e93ddce51f1dd2
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-23 14:01:01 +0530

    mm

--- pages/AI_Civil_Engineer.py
+++ pages/AI_Civil_Engineer.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.components.v1.html(
     """
     <iframe 
-        src="https://montys-multipdf-chat.streamlit.app/?embed=true,embed_options=disable_scrolling,dark_theme" 
-        style="height: 800px; width: 100%; border: none;">
+        src="https://montys-multipdf-chat.streamlit.app/?embed=true" 
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;">
     </iframe>
     """,
-    height=800,
+    height=0,  # Height is controlled entirely by CSS
 )
