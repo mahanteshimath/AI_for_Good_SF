94dadf1d01b462caf9b1e4694802b93cedbaf2bd
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-24 17:06:31 +0530

    V01_CIVIL_AVIATION_PASSENGER

--- pages/Civil_Aviation_Analysis_Prediction.py
+++ pages/Civil_Aviation_Analysis_Prediction.py
@@ -36,8 +36,7 @@ def execute_query(query):
 
 
 
-Q1=f'''
-SELECT CIVIL_AVIATION_PASSENGER_CATEGORY, "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019" FROM IND_DB.IND_SCH.T01_CIVIL_AVIATION_PASSENGER'''
+Q1=f'''SELECT * FROM  IND_DB.IND_SCH.V01_CIVIL_AVIATION_PASSENGER'''
 R1 = execute_query(Q1)
 r1_expander = st.expander("Data sets used in this entire analysis.")
 R1_DF = pd.DataFrame(R1)
