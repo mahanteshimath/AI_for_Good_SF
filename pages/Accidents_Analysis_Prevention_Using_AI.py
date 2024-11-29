092909b28a852c0df5373b7133a8916b68c7c653
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-11-29 20:34:41 +0530

    Update Accidents_Analysis_Prevention_Using_AI.py

--- pages/Accidents_Analysis_Prevention_Using_AI.py
+++ pages/Accidents_Analysis_Prevention_Using_AI.py
@@ -132,7 +132,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 # Key Metrics in 2022
 st.subheader("Accidents Key Metrics 2022 (2019 vs 2022)")
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4 = st.columns(4,gap="small")
 with col1:
     st.metric("Total Accidents", "394,163", "12.5% ↑")
 with col2:
